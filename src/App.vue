a337647c09cccba55f42f147aa4732c958434d6c
establ1sh <3780067483@qq.com>
2025-07-21 13:40:25 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
-<div v-if="sharedState.vis">
+<div>
   <div v-if = "$route.meta.showNav !== false">
   <router-link to ="/studio"><h1>（仮）根据工作室分析</h1></router-link>
   </div>
   <router-view />
 </div>
   <div>
-    <button @click="changevisible" v-if="sharedState.vis">排行</button>
-    <rankingpage v-if="!sharedState.vis"></rankingpage>
+  <div v-if = "$route.meta.showNav !== false">
+  <router-link to ="/rank"><h1>动画列表</h1></router-link>
+  </div>
+  <router-view />
   </div>
 </template>
 
-<script setup>
-  import {ref} from 'vue'
-    import rankingpage from './components/rankingpage.vue'
-    import {sharedState} from './components/sharedvis.js'
-    function changevisible(){
-        sharedState.vis=!sharedState.vis
-    }
+<script>
 </script>
 
 <style scoped>
