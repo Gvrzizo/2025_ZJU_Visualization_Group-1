0d3aebdb669e03e8c507f3a2a6ac57cfff7e8d60
establ1sh <3780067483@qq.com>
2025-07-21 13:40:49 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import byStudio from '@/components/byStudio.vue'
+import Rankingpage from '@/components/rankingpage.vue';
 
 const routes = [
   {
     path: '/studio',
     name: 'byStudio',
     component: byStudio,
+    meta: {
+      showNav: false
+    }
+  },
+  {
+    path: '/rank',
+    name: 'rankingpage',
+    component: Rankingpage,
     meta: {
       showNav: false
     }
